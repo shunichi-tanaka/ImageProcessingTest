5dfebada1320b82c5d10bfb17875114a30ffd12e
田中舜一 <seihoushunichi@gmail.com>
2017-06-16 23:41:22 +0900

    add const

--- ImageProcessingTest/Const.swift
+++ ImageProcessingTest/Const.swift
@@ -13,5 +13,6 @@ struct Const{
     
     let TestText = "This is test text!"
     let testimage = UIImage(named:"neko.jpg")!.mask(image: UIImage(named: "mask.jpg"))
+    let font = UIFont.boldSystemFont(ofSize: 20)
     
 }
